370c43b134dafa141bcb0420fcc09c4f5c58a3f1
Arthur Araújo <arthuroaraujo@gmail.com>
2024-03-03 14:34:07 -0300

    feat: criada a sidebar e roteamento

---
+++ src/components/SideBar/SideBar.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import SidebarLink from '@/components/SideBar/SideBarLink.vue'
+
+export default {
+  components: { SidebarLink },
+}
+</script>
+
+<template>
+  <div class="sidebar">
+    <h1>
+      <span>Meteora</span>
+    </h1>
+
+    <SidebarLink to="/" icon="fas fa-home">Início</SidebarLink>
+    <SidebarLink to="/temperatura" icon="fas fa-columns">Temperatura</SidebarLink>
+    <SidebarLink to="/vento" icon="fas fa-chart-bar">Vento</SidebarLink>
+  </div>
+</template>
+
+<style>
+:root {
+  --sidebar-bg-color: #2f855a;
+  --sidebar-item-hover: #38a169;
+  --sidebar-item-active: #276749;
+}
+</style>
+
+<style scoped>
+@import '@/styles/sideBar.css';
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import { registerPlugins } from '@/plugins'
 
-// Components
-import App from './App.vue'
+import TemperaturePage from './views/TemperaturePage.vue'
+import WindPage from './views/WindPage.vue'
+import HomePage from './views/HomePage.vue'
 
-// Composables
-import { createApp } from 'vue'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/temperatura', component: TemperaturePage },
+    { path: '/vento', component: WindPage },
+  ]
+})
 
 const app = createApp(App)
 
 registerPlugins(app)
 
+app.use(router)
+
 app.mount('#app')
+
