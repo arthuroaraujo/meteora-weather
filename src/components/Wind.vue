1e76800533326557e8742d15c12a42dba063cc21
Arthur Araújo <arthuroaraujo@gmail.com>
2024-03-03 14:42:43 -0300

    feat: implementacao das tabelas em temperature e wind

--- src/components/Wind.vue
+++ src/components/Wind.vue
@@ -1,15 +1,95 @@
 <template>
-    <v-card elevation="2">
-      <div class="wind">
-        <h2>Wind Component</h2>
+  <v-card elevation="2">
+    <div class="vento">
+      <div class="date-inputs">
+        <input type="date" class="custom-date-input" v-model="startDate">
+        <input type="date" class="custom-date-input" v-model="endDate">
       </div>
-    </v-card>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    name: 'Wind',
-  });
-  </script>
+      <button class="custom-button" @click="fetchWeatherData">Buscar Dados de Vento</button>
+
+      <v-data-table :items="items" :headers="headers"></v-data-table>
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { fetchWeatherData } from '@/services/OpenMeteoService';
+import { validateDates } from '@/utils/utils';
+import { WindItem } from '@/interface/WindItem';
+
+export default defineComponent({
+  name: 'WindComponent',
+  props: {
+    startDate: {
+      type: String, 
+      required: true 
+    },
+    endDate: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      startDate: '2024-01-01',
+      endDate: '2024-01-01',
+      items: [] as WindItem[],
+      headers: [
+        {title: 'Data', value: 'dateTime', align: 'center'},
+        {title: 'Velocidade do Vento a 10m em km/h', value: 'wind_speed_10m', align: 'center'},
+        {title: 'Velocidade do Vento a 80m em km/h', value: 'wind_speed_80m', align: 'center'},
+      ] as any,
+      weatherData: null as any,
+      data: null as any,
+    };
+  },
+  methods: {
+    async fetchWeatherData() {
+      const validationMessage = validateDates(this.startDate, this.endDate);
+      if (validationMessage) {
+        alert(validationMessage);
+        return;
+      }
+      try {
+        const responses = await fetchWeatherData({
+          startDate: this.startDate,
+          endDate: this.endDate
+        });
+        this.weatherData = responses;
+        
+        if (!this.weatherData?.hourly) {
+          console.error('Dados da resposta da API estão incompletos:', this.weatherData);
+          return
+        }
+
+        const list: WindItem[] = [];
+        const dateTime = this.weatherData.hourly.time;
+        const windSpeed10m = this.weatherData.hourly.windSpeed10m;
+        const windSpeed80m = this.weatherData.hourly.windSpeed80m;
+        
+        
+        windSpeed10m.forEach((el: any, index: number) => {
+          const dateObject = new Date(dateTime[index]);
+          const formattedDateTime = dateObject.toLocaleString('pt-BR', { timeZone: 'UTC' });
+
+          const item: WindItem = {
+            dateTime: formattedDateTime,
+            wind_speed_10m: el.toFixed(2),
+            wind_speed_80m: windSpeed80m[index].toFixed(2)
+          };
+
+          list.push(item);
+        });
+
+        this.items = list;
+
+        this.$emit('update:startDate', this.startDate);
+        this.$emit('update:endDate', this.endDate);
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    }
+  }
+});
+</script>
--- src/views/TemperaturePage.vue
+++ src/views/TemperaturePage.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>
-      <h1>Temperature Page</h1>
-    </div>
-  </template>
+  <div class="temperatura-page">
+    <h1>Temperatura</h1>
+    <Temperature :start-date.sync="startDate"
+      :end-date.sync="endDate"/>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'TemperaturePage'
+<script lang="ts">
+import { fetchWeatherData } from '@/services/OpenMeteoService';
+import Temperature from '../components/Temperature.vue';
+
+
+export default {
+  components: {
+    Temperature,
+},
+  data() {
+    return {
+      startDate: '',
+      endDate: ''
+    }
+  },
+  methods: {
+    async fetchMethod() {
+      try {
+        const response = await fetchWeatherData({
+          startDate: this.startDate,
+          endDate: this.endDate});
+      } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+      }
+    }
   }
-  </script>
-  
+}
+</script>
